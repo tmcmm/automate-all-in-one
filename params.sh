4e9ae02e6eb13a81c189e63660463d238228cbfd
tmcmm <tiagocorreia@microsoft.com>
2023-01-02 18:00:57 +0000

    private cluster

--- params.sh
+++ params.sh
@@ -1,10 +1,10 @@
 ## GENERAL VARIABLES
-AKS_NAME="aks"
+PURPOSE=""
 AKS_RG_LOCATION="westeurope"
 AKS_VERSION="1.23.8"
 AKS_VNET_2_OCTETS="10.4"   # Define the fisrt 2 octets for Vnet
 AKS_ZONES="1 2 3"          # Define AKS Zones
-AKS_2ND_NP_ZONES="1 2 3"       # Defines NP Zones
+AKS_2ND_NP_ZONES="1 2 3"   # Defines NP Zones
 
 ## AKS Vnet Settings
 AKS_VNET_CIDR="$AKS_VNET_2_OCTETS.0.0/16"
@@ -20,12 +20,17 @@ AKS_HAS_AUTO_SCALER="0"       # 1 = AKS has Auto Scaler enabled
 AKS_HAS_MANAGED_IDENTITY="0"  # 1 = AKS has Managed Identity enabled
 AKS_HAS_NETWORK_POLICY="0"    # 1 = AKS has Azure Net Pol enabled
 AKS_HAS_2ND_NODEPOOL="1"      # 1 = AKS has second npool
-AKS_HAS_JUMP_SERVER="0"       # 1 = Deploy Linux Jump Server
+AKS_CREATE_JUMP_SERVER="1"    # 1 = If we need to create a JS from Other Vnet 
+AKS_HAS_JUMP_SERVER="0"       # 1 = If we already have a Jump Server from Other Vnet, with Peered Vnet 
 
+#########################################################################
+## If AKS_HAS_JUMP_SERVER="1" then we need to define the setting below
+#########################################################################
+EXISTING_JUMP_SERVER_VNET_NAME="vnet-vm-jpsrv"
 
 ## AKS Specifics
-AKS_RG_NAME="rg-aks-"$AKS_NAME
-AKS_CLUSTER_NAME="aks-"$AKS_NAME
+AKS_RG_NAME="rg-aks-"$PURPOSE
+AKS_CLUSTER_NAME="aks-"$PURPOSE
 AKS_SYS_NP_NODE_SIZE="Standard_D4s_v3"
 AKS_USR_NP_NODE_SIZE="Standard_D4s_v3"
 AKS_SYS_NP_NODE_COUNT="1"
@@ -43,7 +48,7 @@ OS_SKU="Ubuntu"   # CBLMariner, Ubuntu, Windows2019, Windows2022
 AKS_CNI_PLUGIN="azure"
 AKS_VNET="vnet-"$AKS_CLUSTER_NAME
 AKS_SNET="snet-"$AKS_CLUSTER_NAME
-#AKS_NET_NPOLICY="azure"   # calico or azure
+AKS_NET_NPOLICY="azure"   # calico or azure
 
 
 ## My ISP PIP
@@ -74,4 +79,30 @@ JS_VM_OS_DISK_NAME="$JS_VM_NAME""_disk_01"
 JS_VM_NSG_NAME="$JS_VM_NAME""_nsg"
 JS_VM_NIC_NAME="$JS_VM_NAME""nic01"
 JS_TAGS="env=kubernetes"
+
+## VM Settings
+LJ_LOCATION="$AKS_RG_LOCATION"
+LJ_VNET="vnet-users"
+LJ_RG="rg-vm-$AKS_CLUSTER_NAME"
+LJ_VNET_CIDR="$LJ_VNET_PREFIX.0.0/24"
+LJ_SNET="subnet-users"
+LJ_SNET_CIDR="$LJ_VNET_PREFIX.0.0/28"
+LJ_PRIV_IP="$LJ_VNET_PREFIX.0.4/32"
+LJ_NAME="lclt-"$AKS_NAME
+LJ_INTERNAL_NAME="lclt-"$AKS_NAME
+LJ_IMAGE_PROVIDER="Canonical"
+LJ_IMAGE_OFFER="UbuntuServer"
+LJ_IMAGE_SKU="18.04-LTS"
+LJ_IMAGE_VERSION="latest"
+LJ_IMAGE="$LJ_IMAGE_PROVIDER:$LJ_IMAGE_OFFER:$LJ_IMAGE_SKU:$LJ_IMAGE_VERSION"
+LJ_SIZE="Standard_D2s_v3"
+LJ_OS_DISK_SIZE="40"
+LJ_PIP="$LJ_NAME-pip"
+LJ_DEFAULT_IP_CONFIG="ipconfig1"
+LJ_STORAGE_SKU="Standard_LRS"
+LJ_AUTH_TYPE="ssh"
+LJ_NSG_NAME="$LJ_NAME""_nsg"
+LJ_NIC_NAME="$LJ_NAME""nic01"
+LJ_OS_DISK_NAME="$LJ_NAME""_disk_01"
+LJ_TAGS="env=jump-server"
 
